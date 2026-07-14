28230421b6867aea5b58bee89680f816cfd21e18
Any Margallo <andyraill14@gmail.com>
2026-07-14 17:24:22 +0800

    grade calculator update 1

---
+++ gradecalculator.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+float ask_for_name () {
+    float name;
+    cout << "Ener name of student: ";
+    cin >> name;
+    return name;
+}
+
+float ask_for_m1 () {
+    float m1;
+    cout << "Enter Midterm Quiz 1: ";
+    cin >> m1;
+    return m1;
+}
+
+float ask_for_m2 () {
+    float m2;
+    cout << "Enter Midterm Quiz 2: ";
+    cin >> m2;
+    return m2;
+}
+
+float ask_for_m3 () {
+    float m3;
+    cout << "Enter Midterm Quiz 3: ";
+    cin >> m3;
+    return m3;
+}
+
+float ask_for_lab1 () {
+    float lab1;
+    cout << "Enter lab 1: ";
+    cin >> lab1;
+    return lab1;
+}
+
+float ask_for_lab2 () {
+    float lab2;
+    cout << " 2: ";
+    cin >> lab2;
+    return lab2;
+}
+
+float ask_for_m3 () {
+    float m3;
+    cout << "Enter Midterm Quiz 3: ";
+    cin >> m3;
+    return m3;
+}
+
+
+mq1 (midterm quiz 1), mq2, mq3, lab 1, lab2, lab 3, and
+midterm exam. All inputted data should be from 60% up to 100%. Using
+function.
